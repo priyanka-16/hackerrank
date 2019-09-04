f3253ad2d98a724c65a1eb6144f9f58c2aa48adb
priyanka-16 <priyanka-16@users.noreply.github.com>
2019-09-04 13:46:42 +0530

    Create hack01.cpp

---
+++ hack01.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<=b;i++)
+    {
+        if(n>=1 && n<=9)
+        switch(n){
+            case 1:cout<<"one";
+            break;
+            case 2:cout<<"two";
+            break;
+            case 3:cout<<"three";
+            break;
+            case 4:cout<<"four";
+            break;
+            case 5:cout<<"five";
+            break;
+            case 6:cout<<"six";
+            break;
+            case 7:cout<<"seven";
+            break;
+            case 8:cout<<"eight";
+            break;
+            case 9:cout<<"nine";
+            break;
+        
+        }
+        else if(n>9)
+        {
+            if(n%2==0)
+            cout<<even;
+            else cout<<"odd"; 
+            }
+        }
+    }
+    return 0;
+}
+
